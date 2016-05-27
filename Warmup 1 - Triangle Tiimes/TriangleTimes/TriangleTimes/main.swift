537248f825768ac9d4ca65fe5ac4eed72be4f9f3
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-27 09:53:18 -0400

    Initial files for this warmup.

---
+++ Warmup 1 - Triangle Tiimes/TriangleTimes/TriangleTimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TriangleTimes
+//
+//  Created by Russell Gordon on 2016-05-27.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
